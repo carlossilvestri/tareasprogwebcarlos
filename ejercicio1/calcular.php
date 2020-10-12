02d6662ebf3052be4a5ef5484922a9982ab41b87
carlossilvestri <54336195+carlossilvestri@users.noreply.github.com>
2020-10-12 18:11:05 -0400

    Update calcular.php
    Required agregado

--- ejercicio1/calcular.php
+++ ejercicio1/calcular.php
@@ -3,41 +3,69 @@
 include 'assets/inc/layout/header.php';
 include 'assets/inc/funciones/funciones.php';
 ?>
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-auto mr-auto">
-            <div class="card text-left" id="contenido">
-                <div class="card-body">
-                    <h4 class="card-title">La hipotenusa del triangulo rectangulo es: </h4>
-                    <div class="form-group">
-                        <label for="lado1">Hipotenusa</label>
-                        <input disabled type="text" class="form-control" id="lado1" name="lado1"
-                            value="<?php echo $hipotenusa; ?> cm">
-                    </div>
-                    <button type="button" id="btnRegresar" class="btn btn-primary">Regresar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 <div class="container">
     <div class="row">
         <div class="col s12 m8">
-            <div class="card small">
-                <div class="container">
-                    <div class="row">
-                        <div class="col s12">
-                            <h5>La hipotenusa del triangulo rectangulo es: </h5>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input disabled value="<?php echo $hipotenusa; ?> cm" id="disabled" type="text"
-                                        class="validate">
-                                    <label for="disabled">Resultado</label>
+            <div class="container">
+                <div class="row">
+                    <div class="col s12">
+                        <?php $index = 0; ?>
+                        <?php foreach ($arreglo as $array) :  ?>
+                        <?php $index++; ?>
+                        <div class="card small">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="row">
+                                        <br>
+                                        <h5>Datos del Empleado</h5>
+                                        <br>
+                                        <h6>Empleado <?php echo $index; ?>:</h6>
+                                        <div class="row">
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled type="text" name="first_names1" class="validate"
+                                                        value="<?php echo $array["nombres"]; ?>">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled name="last_names1" type="text" class="validate"
+                                                        value="<?php echo $array["apelldios"]; ?>">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled type="number" name="cedula1" class="validate"
+                                                        value="<?php echo $array["cedulas"]; ?>">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled type="text" class="validate"
+                                                        value="<?php echo $array["lugar"]; ?>">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled type="text" class="validate"
+                                                        <?php foreach ($departamentos as $departamento) :  ?>
+                                                        <?php 
+                                                            if ($array["departamento"] == $departamento["value"] ){ echo "value='".$departamento["escrito"] . "' "; } ?>
+                                                        <?php endforeach; ?>>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="input-field col s12">
+                                                    <input disabled type="number" class="validate"
+                                                        value="<?php echo $array["sueldo"]; ?>">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <button type="button" id="btnRegresar"
-                                    class="waves-effect waves-light btn">Regresar</button>
                             </div>
                         </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
